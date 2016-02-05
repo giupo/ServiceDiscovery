fdb324f8de6c431e978c900ede7070d197a8ae38
Giuseppe Acito <giuseppe.acito@gmail.com>
2016-02-05 16:51:49 +0100

    Bump version: 0.0.4 → 0.0.5

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ test_requirements = [
 
 setup(
     name='pysd',
-    version='0.0.4',
+    version='0.0.5',
     description='Python Service Discovery and Registry',
     long_description=readme + '\n\n' + history,
     author='Giuseppe Acito',
