452420f8ff7be42b46fd3bd8c1a4234b7db465d7
Giuseppe Acito <giuseppe.acito@gmail.com>
2016-11-02 15:44:13 +0100

    Bump version: 0.1.0 → 0.1.1

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ test_requirements = [
 
 setup(
     name='ServiceDiscovery',
-    version='0.1.0',
+    version='0.1.1',
     description='Python Service Discovery and Registry',
     long_description=readme + '\n\n' + history,
     author='Giuseppe Acito',
