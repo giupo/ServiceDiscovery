ff10a196f975f2043b2bd2b28a3e0c1323e9eaff
Giuseppe Acito <giuseppe.acito@bancaditalia.it>
2016-12-12 16:18:06 +0100

    Bump version: 0.1.3 → 0.1.4

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ test_requirements = [
 
 setup(
     name='ServiceDiscovery',
-    version='0.1.3',
+    version='0.1.4',
     description='Python Service Discovery and Registry',
     long_description=readme + '\n\n' + history,
     author='Giuseppe Acito',
